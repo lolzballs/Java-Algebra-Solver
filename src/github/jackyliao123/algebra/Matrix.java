e9d9f7dc8f67b45efc9368d49045093f4771823a
jackyliao123 <jackyliao2008@gmail.com>
2013-12-28 19:54:58 -0500

    Source Files

---
+++ src/github/jackyliao123/algebra/Matrix.java
@@ -0,0 +1,71 @@
+package github.jackyliao123.algebra;
+
+import java.io.Serializable;
+
+/**
+ * Used for solving the parsed equations
+ * @author jackyliao123
+ */
+
+public class Matrix implements Serializable{
+	private static final long serialVersionUID = 9060645754514833245L;
+	private Rational[][] matrix;
+	private int n;
+	public Matrix(Rational[][] rational, int n){
+		this.matrix = rational;
+		this.n = n;
+	}
+	public Rational[] solve(){
+		for (int k = 0; k < n; k++){
+			if (matrix[k][k].equals(0)){
+				changeRow(n, k, matrix);
+			}
+			for (int i = 0; i < n; i++){
+				Rational temp = matrix[i][k];
+				for (int j = 0; j < n + 1; j++){
+					if (i < k)
+						break;
+					if (temp.equals(0))
+						continue;
+					if (!temp.equals(1)){
+						matrix[i][j] = matrix[i][j].divide(temp);
+					}
+					if (i > k)
+						matrix[i][j] = matrix[i][j].subtract(matrix[k][j]);
+				}
+			}
+		}
+		Rational[] result = new Rational[n];
+		for (int i = n - 1; i >= 0; i--){
+			Rational temp = matrix[i][n];
+			for (int j = n - 1; j >= 0; j--){
+				if (i < j && !matrix[i][j].equals(0)){
+					temp = temp.subtract(result[j].multiply(matrix[i][j]));
+				}
+			}
+			temp = temp.divide(matrix[i][i]);
+			result[i] = temp;
+		}
+		return result;
+	}
+	private void changeRow(int n, int k, Rational[][] matrix){
+		Rational[] temp = new Rational[n + 1];
+		for (int i = k; i < n; i++){
+			if (i + 1 == n && matrix[k][k].equals(0)){
+				if (matrix[k][n].equals(0)){
+					throw new InfiniteSolutionException();
+				}
+				else{
+					throw new NoSolutionException();
+				}
+			}
+			for (int j = 0; j < n + 1; j++){
+				temp[j] = matrix[k][j];
+				matrix[k][j] = matrix[i + 1][j];
+				matrix[i + 1][j] = temp[j];
+			}
+			if (!matrix[k][k].equals(0))
+				return;
+		}
+	}
+}
